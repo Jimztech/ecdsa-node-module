2dcd767405975725dc43c7ab1de8f80a4757b3ac
Jimztech <rconor66@gmail.com>
2024-03-18 00:37:44 +0100

    Ecdsa project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 
 function App() {
   const [balance, setBalance] = useState(0);
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useState("0x1c6d0e");
+  const [privateKey, setPrivateKey] = useState("d3a0c49a7126a06590123cef25376935365a8e4a6c807fb887603cb6ee3b1e1e")
 
   return (
     <div className="app">
@@ -14,6 +15,8 @@ function App() {
         setBalance={setBalance}
         address={address}
         setAddress={setAddress}
+        privateKey={privateKey}
+        setPrivateKey={setPrivateKey}
       />
       <Transfer setBalance={setBalance} address={address} />
     </div>
